0f0832c5b0d4ae59ac635d1312026e3e9139236b
cGradying <cGradying@gmail.com>
2026-09-04 00:51:59 +0800

    Remove legacy root asset paths

--- home.js
+++
@@ -1,7 +0,0 @@
-const iconButtons = document.querySelectorAll(".icon-button");
-
-window.addEventListener("load", () => {
-    iconButtons.forEach((button) => {
-        button.classList.add("reveal");
-    });
-});
